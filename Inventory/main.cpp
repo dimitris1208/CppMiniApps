aef42e479603f7b5abe43552b2b7be24f7f0e1fb
Dimitris Stragalinos <75848765+dimitris1208@users.noreply.github.com>
2023-11-10 03:14:41 +0200

    Update main.cpp

--- Inventory/main.cpp
+++ Inventory/main.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include "Item.h"
+using namespace std;
+
+int main()
+{
+    vector<Item> receipt ;
+    string name,category ;
+    int code ;
+
+
+    while(true){
+        cout<<"Add an element to the receipt or exit by typing '0' \n";
+        cout<<"Name: " ;
+        cin>>name;
+
+        if (name[0] == '0'){
+            break;
+        }
+        cout<<"Category: ";
+        cin>>category ;
+        cout<<"Code: " ;
+        cin>>code ;
+        Item * current_Item = new Item(name,category,code);
+        receipt.push_back(* current_Item);
+    }
+
+    for(int i = 0 ; i < receipt.size() ; i++){
+            cout<<"\n"<<receipt[i].getName();
+        }
+
+    return 0;
+}
 
